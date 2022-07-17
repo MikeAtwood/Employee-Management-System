d8ab1dafdc1838d9059ce87016eb1dad37c0a288
Deadpool1 <atwood169@yahoo.com>
2022-07-17 18:20:51 -0500

    added to role and employee table

--- db/seeds.sql
+++ db/seeds.sql
@@ -15,3 +15,14 @@ INSERT INTO role (title, department_id, salary)
         ('Accountant', 2, 125000),
         ('Legal Team Lead', 3, 250000),
         ('Lawyer', 3, 190000);
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+    VALUES
+        ('John', 'Doe', 1, null),
+        ('Mike', 'Chan', 2, 1),
+        ('Ashley', 'Rodrigues', 3, null),
+        ('Kevin', 'Tupkin', 4, 3),
+        ('Kunal', 'Singh' 5, null),
+        ('Malia', 'Brown', 6. 5),
+        ('Sarah', 'Lourd', 7, null),
+        ('Tom', 'Allen', 8, 7);
