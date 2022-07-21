a720653a6ab17ff5bae7cd049e9810d22de899b9
Deadpool1 <atwood169@yahoo.com>
2022-07-20 19:49:00 -0500

    finished

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const cTable = require('console.table');
 const connection = mysql.createConnection({
     host: 'localhost',
     user: 'root',
-    password: 'Deadpool1',
+    password: '',
     database: 'employee'
 });
 
